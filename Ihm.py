ef3186b52deca4c6955cf407896d517ce8e4d955
Vincent LAMBERT <v.lambert@natuition.com>
2022-05-24 08:47:01 +0200

    Add basic serv

--- Ihm.py
+++ Ihm.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template
-import multiprocessing
-from typing import Callable
+import threading
 
 class WebServer:
 
-    def __init__(self, server_dir: str):
-        self.__app = Flask("webserver")
+    def __init__(self):
+        self.__app = Flask("webserver", static_folder="./templates")
         self.__app.add_url_rule('/', view_func=self.__index)
 
     def run_server(self, host: str, port: int):
-        self.__webStream = multiprocessing.Process(target=self.__app.run, args=(host, port, False))
+        self.__webStream = threading.Thread(target=self.__app.run, args=(host, port, False))
         self.__webStream.start()
 
     def __index(self):
         return render_template("index.html")
+
+
+def main():
+    webServer = WebServer()
+    webServer.run_server("0.0.0.0", 80)
+
+if __name__ == "__main__":
+    main()
