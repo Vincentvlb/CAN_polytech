941ebf7bca47906aed4cc2c295cb850d613afb38
amoreau <aline.moreau@alpma.fr>
2022-05-24 17:09:53 +0200

    commit2

--- Couleur.py
+++ Couleur.py
@@ -1,7 +1,16 @@
 from Capteur import Capteur
 
 class Couleur (Capteur):
-    
+    def _init(self):
+        self._valeur_possible = ["Rouge", "Blanc", "Vert", "Bleu"]
+
     def traduction_IHM(self):
-        valeur = str(self._valeur_voulue)
+        if (self._valeur_voulue == 2): 
+            valeur = self._valeur_possible[1]
+        elif (self._valeur_voulue == 3):
+            valeur = self._valeur_possible[0]
+        elif (self._valeur_voulue == 4):
+            valeur = self._valeur_possible[2]
+        else :
+            valeur = self._valeur_possible[3]
         return valeur
